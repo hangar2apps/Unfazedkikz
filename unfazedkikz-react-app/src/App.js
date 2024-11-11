adfd4cac3355f3c4ec2e79ab325b207fcc8d76f6
Bryan Rigsby <hangar2apps@gmail.com>
2024-11-11 11:57:59 -0600

    Update App.js

--- unfazedkikz-react-app/src/App.js
+++ unfazedkikz-react-app/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
