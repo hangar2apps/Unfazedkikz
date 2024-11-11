5125c8efaf28732c9ecbe9510160b9bc79a7d2a0
Bryan Rigsby <hangar2apps@gmail.com>
2024-11-10 20:42:11 -0600

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,19 +7,19 @@ function Footer() {
         <div className="row">
           <div className="col-md-4 mb-4">
             <h3 className="h5 mb-3">Quick Links</h3>
-            <ul className="list-unstyled">
+            {/* <ul className="list-unstyled">
               <li><a href="#" className="text-light">About Us</a></li>
               <li><a href="#" className="text-light">Contact</a></li>
               <li><a href="#" className="text-light">FAQ</a></li>
-            </ul>
+            </ul> */}
           </div>
           <div className="col-md-4 mb-4">
             <h3 className="h5 mb-3">Connect</h3>
-            <ul className="list-unstyled">
+            {/* <ul className="list-unstyled">
               <li><a href="#" className="text-light"><i className="fab fa-instagram me-2"></i>Instagram</a></li>
               <li><a href="#" className="text-light"><i className="fab fa-twitter me-2"></i>Twitter</a></li>
               <li><a href="#" className="text-light"><i className="fab fa-facebook me-2"></i>Facebook</a></li>
-            </ul>
+            </ul> */}
           </div>
           <div className="col-md-4 mb-4">
             <h3 className="h5 mb-3">Newsletter</h3>
