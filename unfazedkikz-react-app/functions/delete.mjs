51df8f11fc2a292bdd1782082a9a12fa46ccedcd
Bryan Rigsby <hangar2apps@gmail.com>
2024-11-16 18:08:35 -0600

    wip

--- unfazedkikz-react-app/functions/delete.mjs
+++ unfazedkikz-react-app/functions/delete.mjs
@@ -24,7 +24,7 @@ export default async (req, context) => {
       }
 
       const shoesStore = getStore({ name: 'shoes', siteID: siteID, token: token });
-      await shoesStore.delete('New Balance/90609060/Rain Loud GreyRose Lime');
+      await shoesStore.delete('New Balance/90609060/Rose LimeSlate Grey');
 
 
 
