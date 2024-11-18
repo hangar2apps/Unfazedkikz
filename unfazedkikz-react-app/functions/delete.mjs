15135cac6dad23650ceb74663a10597d02150c66
Bryan Rigsby <hangar2apps@gmail.com>
2024-11-17 19:45:10 -0600

    wip

--- unfazedkikz-react-app/functions/delete.mjs
+++ unfazedkikz-react-app/functions/delete.mjs
@@ -24,7 +24,7 @@ export default async (req, context) => {
       }
 
       const shoesStore = getStore({ name: 'shoes', siteID: siteID, token: token });
-      await shoesStore.delete('New Balance/9060/Rain Loud Grey');
+      await shoesStore.delete("New Balance/990/Levi's Blue");
 
 
 
--- unfazedkikz-react-app/src/App.js
+++ unfazedkikz-react-app/src/App.js
@@ -26,7 +26,75 @@ function App() {
       }
     };
 
-    getShoes();
+    // getShoes();
+
+    // for testing 
+    setShoeBrands(["New Balance", 'Asics']);
+    setShoes([
+      {
+        ID: 1,
+        ShoeBrand: 'New Balance',
+        ShoeLine: '9060',
+        ShoeModel: 'Rain Loud Grey',
+        URL: 'https://raw.githubusercontent.com/hangar2apps/Unfazedkikz/v1/shoes/New%20Balance/9060/Rain%20Cloud%20Grey.jpg',
+      },
+      {
+        ID: 2,
+        ShoeBrand: 'New Balance',
+        ShoeLine: '9060',
+        ShoeModel: 'Arctic Grey',
+        URL: 'https://raw.githubusercontent.com/hangar2apps/Unfazedkikz/v1/shoes/New%20Balance/9060/Arctic%20Grey.jpg',
+      },
+      {
+        ID: 3,
+        ShoeBrand: 'New Balance',
+        ShoeLine: '9060',
+        ShoeModel: 'Beach Glass',
+        URL: 'https://raw.githubusercontent.com/hangar2apps/Unfazedkikz/v1/shoes/New%20Balance/9060/Beach%20Glass.jpg',
+      },
+      {
+        ID: 4,
+        ShoeBrand: 'New Balance',
+        ShoeLine: '9060',
+        ShoeModel: 'Beef and Broccoli',
+        URL: 'https://raw.githubusercontent.com/hangar2apps/Unfazedkikz/v1/shoes/New%20Balance/9060/Beef%20and%20Broccoli.jpg',
+      },
+      {
+        ID: 5,
+        ShoeBrand: 'New Balance',
+        ShoeLine: '990',
+        ShoeModel: 'Black White',
+        URL: 'https://raw.githubusercontent.com/hangar2apps/Unfazedkikz/v1/shoes/New%20Balance/990/Black%20White.jpg',
+      },
+      {
+        ID: 6,
+        ShoeBrand: 'New Balance',
+        ShoeLine: '990',
+        ShoeModel: 'Joe Freshgoods',
+        URL: 'https://raw.githubusercontent.com/hangar2apps/Unfazedkikz/v1/shoes/New%20Balance/990/Joe%20Freshgoods.jpg',
+      },
+      {
+        ID: 7,
+        ShoeBrand: 'New Balance',
+        ShoeLine: '990',
+        ShoeModel: "Olive",
+        URL: 'https://raw.githubusercontent.com/hangar2apps/Unfazedkikz/v1/shoes/New%20Balance/990/Olive.jpg',
+      },
+      {
+        ID: 8,
+        ShoeBrand: 'Asics',
+        ShoeLine: 'Gel Kahana',
+        ShoeModel: "TR V4",
+        URL: 'https://raw.githubusercontent.com/hangar2apps/Unfazedkikz/v1/shoes/Asics/Gel%20Kahana/TR%20V4.jpg',
+      },
+      {
+        ID: 9,
+        ShoeBrand: 'Asics',
+        ShoeLine: 'Gel Kahana',
+        ShoeModel: "TR V4 Silver Red",
+        URL: 'https://raw.githubusercontent.com/hangar2apps/Unfazedkikz/v1/shoes/Asics/Gel%20Kahana/TR%20V4%20Silver%20Red.jpg',
+      },
+    ]);
   }, []);
 
   
