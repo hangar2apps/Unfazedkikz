6f8f0148967a5f15f415794d41f288b2f7209961
Bryan Rigsby <hangar2apps@gmail.com>
2024-11-22 21:43:09 -0600

    Update delete.mjs

--- unfazedkikz-react-app/functions/delete.mjs
+++ unfazedkikz-react-app/functions/delete.mjs
@@ -24,12 +24,7 @@ export default async (req, context) => {
       }
 
       const shoesStore = getStore({ name: 'shoes', siteID: siteID, token: token });
-      await shoesStore.delete("Asics /Gel Kayano 14/Silver Black Pink ");
-      await shoesStore.delete("Asics /Gel Quantum Kinetic/Pepper Light Indigo ");
-      await shoesStore.delete("Asics /Metarise 2/Paris");
-      await shoesStore.delete("Asics /Nimbus 26 /Grey ");
-      await shoesStore.delete("Asics /Nimbus 26/Blue");
-      await shoesStore.delete("Asics /Tiger Gel-Kahana /Blue Yellow ");
+      await shoesStore.delete("Asics /Gel Quantum Kinetic /Pepper Light Indigo ");
 
 
 
